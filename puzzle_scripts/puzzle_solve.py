a026c835f61f5bff22e7117449382272dd6a98af
Abishek Padaki <abiks192@gmail.com>
2024-02-14 14:52:13 -0800

    Added the ability to store moves made in the sudoku solver script

--- puzzle_scripts/puzzle_solve.py
+++ puzzle_scripts/puzzle_solve.py
@@ -1,6 +1,6 @@
 import sys
 
-moves_made = []
+moves = []
 
 def is_valid(board, row, col, num, n):
     for x in range(n):
@@ -15,6 +15,7 @@ def is_valid(board, row, col, num, n):
     return True
 
 def solve_sudoku(board, n):
+    global moves
     find = find_empty_location(board, n)
     if not find:
         return True
@@ -24,8 +25,10 @@ def solve_sudoku(board, n):
     for num in range(1, n + 1):
         if is_valid(board, row, col, num, n):
             board[row][col] = num
+            moves.append({'row':row+1, 'col':col+1, 'num':num})
             if solve_sudoku(board, n):
                 return True
+            moves.pop()
             board[row][col] = 0
     return False
 
@@ -37,6 +40,7 @@ def find_empty_location(board, n):
     return None
 
 def puzzle_solver(input_string):
+    global moves
     try:
         dimension, puzzle_str = input_string.split('_')
         n = int(dimension)
@@ -45,19 +49,22 @@ def puzzle_solver(input_string):
 
         puzzle = [int(x) for x in puzzle_str]
         board = [puzzle[i * n:(i + 1) * n] for i in range(n)]
+        moves = []
 
         if solve_sudoku(board, n):
             solved_puzzle_str = ''.join(str(cell) for row in board for cell in row)
-            return f"{dimension}_{solved_puzzle_str}"
+            return f"{dimension}_{solved_puzzle_str}", moves
         else:
             return "No solution exists for the given Sudoku puzzle."
     except Exception as e:
-        return f"Error: {e}"
+        return f"Error: {e}", moves
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python puzzle_solver.py '<dimension>_<puzzle_to_be_solved>'")
     else:
         input_string = sys.argv[1]
-        print(puzzle_solver(input_string))
+        solution, moves_made = puzzle_solver(input_string)
+        print(solution)
+        #print(moves_made)
 
